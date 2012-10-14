81bb7106b232d3f59c9932308a44c4b9a273b08e
Florian Franke <Florian@Franke-s.de>
2012-10-14 21:16:52 +0200

    Klassen zum extrahieren von Informationen aus einem Short-Wert
    Habe eine Klasse mit Konstanten für die Schachfiguren und Methoden zum
    Ermitteln der Namen/der Farbe/der Position der Schachfiguren.
    Außerdem eine Klasse zum Extrahieren der Informationen aus dem
    Short-Wert per BitShift.
    Zusätzliche Klasse zum Ausprobieren.

---
+++ helpful/src/TestingTools.java
@@ -0,0 +1,31 @@
+
+public class TestingTools {
+
+	/**
+	 * Testet das extrahieren von Informationen aus einem Short-Wert
+	 */
+	public static void main(String[] args) {
+		// Schwarzer Kšnig auf C5 (3/5)
+		// 1110011101, zusammengesetzt aus:
+		//   1   - schwarz
+		//   110 - Kšnig (6)
+		//   011 - X-Position (3/C)
+		//   101 - Y-Position (5)
+		short sh = 925;
+		
+		// Figur ermitteln und ausgeben
+		byte figure = ExtractInformationFromBinary.getFigure(sh);
+		System.out.println(ChessfigureConstants.getFigureName(figure));
+		
+		// Position der Figur
+		byte xPos = ExtractInformationFromBinary.getXPosition(sh);
+		System.out.println(ChessfigureConstants.getXPosition(xPos));
+		byte yPos = ExtractInformationFromBinary.getYPosition(sh);
+		System.out.println(yPos);
+		
+		// Farbe der Figur
+		byte color = ExtractInformationFromBinary.getColor(sh);
+		System.out.println(ChessfigureConstants.getFigureColor(color));
+	}
+
+}
