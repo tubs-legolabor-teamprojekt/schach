e23b6db1ce06b2769df0fdbba9b90bab66a36851
Marcel Schubert <m.schubert@tu-bs.de>
2012-10-16 13:50:24 +0200

    ueberfluessige gui entfernt

--- camera/Webcam.java
+++ camera/Webcam.java
@@ -2,9 +2,8 @@ import java.awt.image.BufferedImage;
 import de.humatic.dsj.DSCapture;
 import de.humatic.dsj.DSFilterInfo;
 import de.humatic.dsj.DSFiltergraph;
-import javax.swing.JFrame;
  
-public class Webcam extends JFrame {
+public class Webcam {
 	
 	private BufferedImage bu;
  
@@ -13,8 +12,6 @@ public class Webcam extends JFrame {
     private DSCapture dsc = null;
  
     public Webcam() {
-        super("Spiegel");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         dsFilterInfo = DSCapture.queryDevices();
         dscCapture = new DSCapture[dsFilterInfo[0].length - 1];
         dsc = new DSCapture(DSFiltergraph.D3D9, dsFilterInfo[0][0], false,
@@ -23,8 +20,6 @@ public class Webcam extends JFrame {
         for(int i=0; i<4; i++) {
         bu = dsc.getImage();
     }
-        setLocationRelativeTo(null);
-        this.dispose();
     }
     
     public BufferedImage getImage() {
