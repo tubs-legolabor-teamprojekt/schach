a4f55f69c527836002678baef1cc1a7746b15252
Florian Franke <Florian@Franke-s.de>
2012-10-21 14:14:09 +0200

    Neue Exception-Klasse

---
+++ gamecoordinator/components/FieldException.java
@@ -0,0 +1,20 @@
+package components;
+
+/**
+ * Exception-Klasse der Field-Klasse
+ * @author Florian Franke
+ * 21.10.2012
+ *
+ */
+public class FieldException extends Exception
+{
+	public FieldException()
+	{
+		
+	}
+	
+	public FieldException(String s)
+	{
+		super(s);
+	}
+}
