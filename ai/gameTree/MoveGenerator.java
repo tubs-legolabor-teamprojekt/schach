7dbbfde8450e5e93a6ea1a3be112b4771b360f02
Florian Hallensleben <f.hallensleben@tu-bs.de>
2012-12-08 15:23:36 +0100

    Klasse erstellt, die schließlich die Spielfelder zurückgeben soll...

---
+++ ai/gameTree/MoveGenerator.java
@@ -0,0 +1,20 @@
+package gameTree;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import components.Figure;
+
+/**
+ * Diese Klasse ist zuständig für die Generierung von Zügen aus einem gegebenen Spielfeld.
+ * @author Florian Hallensleben
+ *
+ */
+public class MoveGenerator
+{
+
+    /*private LinkedList<HashMap>(HashMap<Integer, Figure> field, byte colour){
+        
+        return null;
+    }*/
+
+}
