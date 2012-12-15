926258f3573eab9790401f192b31dd7c60666aa8
msschubi <m.schubert@tu-bs.de>
2012-12-15 01:16:07 +0100

    Paar Kommentare zu den Methoden

--- gamecoordinator/game/GameParser.java
+++ gamecoordinator/game/GameParser.java
@@ -3,18 +3,28 @@ package game;
 import java.io.*;
 import java.util.LinkedList;
 
-public class GameParser{
-    private String inputFileName = "text.txt";
+/*
+ * @author Marcel Schubert
+ * 
+ * Klasse für schnelles einlesen von gespielten Spielen 
+ * fuer Testzwecke
+ */
+
+public class GameParser {
     private LinkedList<String> stringList = new LinkedList<String>();
+    String inputFileName;
 
     public GameParser(String inputFileName) {
         this.inputFileName = inputFileName;
     }
 
+    /*
+     * Liest die zuvor durch den Konstruktor uebermittelte Textdatei ein und
+     * liest diese Zeilenweise aus
+     */
     public void read() {
         try {
-            BufferedReader in = new BufferedReader(
-                    new FileReader(inputFileName));
+            BufferedReader in = new BufferedReader(new FileReader(inputFileName));
             String line = null;
             while ((line = in.readLine()) != null) {
                 splitLine(line);
@@ -25,37 +35,26 @@ public class GameParser{
         }
     }
 
+    /*
+     * Filtern der Werte aus dem eingelesenen String sowie anfuegen an
+     * LinkedList
+     * 
+     * Kann modifiziert werden fuer unterschiedliche Notationen ...
+     */
     private String splitLine(String line) {
         String[] geteilt2, geteilt1;
         geteilt1 = line.split("-");
         geteilt2 = geteilt1[0].split(":");
         stringList.addFirst(geteilt1[1].replaceAll(" ", ""));
         stringList.addFirst(geteilt2[1].replaceAll(" ", ""));
-        
+
         return line + "\n";
-    }
-    
-    public LinkedList getMoves() {
-        return stringList;
     }
 
     /*
-    // in Datei "outputFileName" schreiben
-    public void writeText(String text) {
-        try {
-            File file = new File(outputFileName);
-            FileWriter fw = new FileWriter(file);
-            fw.write(text);
-            fw.flush();
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+     * @return LinkedListe<String> mit zuvor eingelesenen Zuegen
+     */
+    public LinkedList getMoves() {
+        return stringList;
     }
-*/
-    
-        //f.read();
-        //f.getMoves();
-    
 }
-
