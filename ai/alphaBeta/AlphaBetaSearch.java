35684b439b708c4c6cad5b8734596bc62a9f2513
msschubi <m.schubert@tu-bs.de>
2013-01-14 21:55:54 +0100

    Fehler behoben, dürfte funktionieren mit neg. bewertungen bei ungerader tiefe und pos. bewertungen bei gerader tiefe. Bei ungerader tiefe scheint mir jedoch noch irgendwo ein fehler zu sein...

--- ai/alphaBeta/AlphaBetaSearch.java
+++ ai/alphaBeta/AlphaBetaSearch.java
@@ -7,51 +7,67 @@ import rating.PseudoRating;
 import useful.PseudoValidMove;
 
 /**
- * Dieses ist der erste Test zum Aplha-Beta-CutOff zur Generierung des Spielbaumes.
- * Der Pseudocode stammt aus Wikipedia und wird nach und nach auf unsere Situation angepasst.
+ * Dieses ist der erste Test zum Aplha-Beta-CutOff zur Generierung des
+ * Spielbaumes. Der Pseudocode stammt aus Wikipedia und wird nach und nach auf
+ * unsere Situation angepasst.
+ * 
  * @author tobi
- *
+ * 
  */
-public class AlphaBetaSearch{ // Nacht erster Ueberlegung nicht Multi-Thread-faehig!!
+public class AlphaBetaSearch { // Nacht erster Ueberlegung nicht
+                               // Multi-Thread-faehig!!
 
-	// ############################################# Instanzvariablen
-	
-	PseudoRating rate = new PseudoRating();
-	PseudoValidMove move = new PseudoValidMove();
-	int count = 0;
-		
-	// ############################################# Alpha-Beta-Cut-Off
+    // ############################################# Instanzvariablen
 
-	public int alphaBeta( HashMap<Integer, String> situation, int tiefe, int alpha, int beta ) {
+    PseudoRating rate = new PseudoRating();
+    PseudoValidMove move = new PseudoValidMove();
+    public double count = 0;
 
-	     System.out.println("tiefe "+tiefe);
+    // ############################################# Alpha-Beta-Cut-Off
 
-		if (tiefe == 0 /* || keineZuegeMehr(spieler)*/ ){
-		    count++;
-			byte rating = rate.rate(situation);
-			 System.out.println(" bewertung "+rating);
-			//System.out.println("zähler "+count);
-		    return (int)rating;
-		}
-		
-		int maxValue = alpha;
-		
-		LinkedList<HashMap<Integer, String>> liste = move.move(situation);	// TODO Liste mit Werten füllen
-		
-		// TODO zur Optimierung: Felder sortieren
-		
-		while( !liste.isEmpty() ){                                            /* TODO Noch Kindsituationen vorhanden*/
-			int value = -alphaBeta(liste.pollFirst(), tiefe-1, -beta, -maxValue);
-			if(value > maxValue){
-				maxValue = value;
-				if(maxValue >= beta){ //Beta-CutOff
-					System.out.println("Cut in Tiefe "+tiefe);
-					break;
-				}
-			}
-		}
-		//System.out.println("maxValue: "+maxValue);
-		return maxValue;// TODO Größter Wert
-	}
-	// ############################################# Weitere Methoden
+    /*
+     * Da NegaMax Variante: Tiefenabhängig. Bewertungen ungerader tiefe:
+     * negativ, Bewertungen gerader Tiefe: positiv.
+     */
+    public int alphaBeta(HashMap<Integer, String> situation, int depth, int player, int alpha, int beta) {
+
+        //System.out.println("tiefe " + depth /* +" alpha: "+alpha+" beta: "+beta */);
+
+        if (depth == 0 /* || keineZuegeMehr(spieler) */) {
+            count++;
+            byte rating = rate.rate(situation, player);
+            //System.out.println(" bewertung " + rating);
+            //System.out.println("zähler "+count);
+            return (int) rating;
+        }
+
+        int maxValue = alpha;
+
+        LinkedList<HashMap<Integer, String>> liste = move.move(situation); // TODO
+                                                                           // Liste
+                                                                           // mit
+                                                                           // Werten
+                                                                           // füllen
+
+        // TODO zur Optimierung: Felder sortieren
+
+        while (!liste.isEmpty()) { /* TODO Noch Kindsituationen vorhanden */
+            int value = -alphaBeta(liste.pollFirst(), depth - 1, -player, -beta, -maxValue);
+            if (value > maxValue) { // hier nicht ">=", weil: Wenn es keine
+                                    // Verbesserung ist, brauch ich nicht
+                                    // schauen...
+                maxValue = value;
+                if (maxValue >= beta) { // Beta-CutOff //hier ">=", weil: hier
+                                        // kann ich abbrechen, bei gleichheit.
+                                        // bei value>=maxvalue hätte zuvor ein
+                                        // cutoff stattgefunden
+                    //System.out.println("Cut in Tiefe " + depth);
+                    break;
+                }
+            }
+        }
+        // System.out.println("maxValue: "+maxValue);
+        return maxValue;// TODO Größter Wert
+    }
+    // ############################################# Weitere Methoden
 }
--- ai/useful/PseudoValidMove.java
+++ ai/useful/PseudoValidMove.java
@@ -6,42 +6,45 @@ import java.util.LinkedList;
 /**
  * Pseudoklasse zur ValidMove
  * 
- * Diese Klasse sollte instanziierbar bleiben, um mehrere Threads versorgen zu können.
+ * Diese Klasse sollte instanziierbar bleiben, um mehrere Threads versorgen zu
+ * können.
  * 
  * @author tobi
- *
+ * 
  */
 public class PseudoValidMove {
 
-	/**
-	 * Gibt eine Lite mit 10 identischen Elementen zurück...
-	 * Nur zum Test der Alpha-Beta-Suche
-	 * @param list
-	 * @return
-	 */
-	public LinkedList<HashMap<Integer, String>> move (HashMap<Integer, String> list){
-		
-		/*
-		 * Erstellt eine neue Liste, die vom generischen Typen ABTree ist
-		 */
-		LinkedList<HashMap<Integer, String>> liste = new LinkedList<HashMap<Integer, String>>();
-		
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
-		
-		/*
-		 * die übergebene Situation wird vervielfältigt und in die neue Liste gespeichert 
-		 */
+    /**
+     * Gibt eine Lite mit 10 identischen Elementen zurück... Nur zum Test der
+     * Alpha-Beta-Suche
+     * 
+     * @param list
+     * @return
+     */
+    public LinkedList<HashMap<Integer, String>> move(HashMap<Integer, String> list) {
 
-		for(int i = 0 ; i < 3 ; i++){
+        /*
+         * Erstellt eine neue Liste, die vom generischen Typen ABTree ist
+         */
+        LinkedList<HashMap<Integer, String>> liste = new LinkedList<HashMap<Integer, String>>();
 
-			liste.add(map);
-		}
-		
-		/*
-		 * Die nun gefülte Liste mit 10 identischen Situationen wird 
-		 * an die aufrufende Methode zurückgegeben
-		 */
-		return liste;
-	}
-	
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+        /*
+         * die übergebene Situation wird vervielfältigt und in die neue Liste
+         * gespeichert
+         */
+
+        for (int i = 0; i < 40; i++) {
+
+            liste.add(map);
+        }
+
+        /*
+         * Die nun gefülte Liste mit 10 identischen Situationen wird an die
+         * aufrufende Methode zurückgegeben
+         */
+        return liste;
+    }
+
 }
