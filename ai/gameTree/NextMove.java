290069b57635f8a9f765bbeb3194c9ef673808a2
Robi-Tobi <t.hundt@tu-braunschweig.de>
2013-03-12 18:01:08 +0100

    Neue Klasse, die den Aufruf entgegen nimmt.

---
+++ ai/gameTree/NextMove.java
@@ -0,0 +1,12 @@
+package gameTree;
+
+import alphaBeta.AlphaBetaSearch;
+
+public class NextMove {
+    
+    public NextMove(){
+        AlphaBetaSearch search = new AlphaBetaSearch();
+        
+    }
+    
+}
