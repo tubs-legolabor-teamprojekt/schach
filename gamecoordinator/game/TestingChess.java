cf0f9d7cff45080ef6a2fbfb0cedfec08ea40f1c
Florian Franke <Florian@Franke-s.de>
2013-05-03 12:22:42 +0200

    Spielsituation eingebaut, nach der der Gegner nach einem Zug schachmatt ist (g4 > h6)

--- gamecoordinator/game/TestingChess.java
+++ gamecoordinator/game/TestingChess.java
@@ -48,10 +48,38 @@ public class TestingChess {
             
             HashMap<Integer, Byte> givenSituation = new HashMap<Integer, Byte>();
             
+            // Situation, bei der der Gegner in einem Zug Schachmatt ist (Weisser Springer von g4 nach h6)
+            givenSituation.put(Field.getFieldNumber("f1"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.ROOK, true));
+            givenSituation.put(Field.getFieldNumber("g1"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.KING, true));
+            givenSituation.put(Field.getFieldNumber("a2"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("b2"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.BISHOP, true));
+            givenSituation.put(Field.getFieldNumber("c2"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("f2"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("h2"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("b3"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("g3"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("g4"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.KNIGHT, true));
+            
+            givenSituation.put(Field.getFieldNumber("d8"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.ROOK, true));
+            givenSituation.put(Field.getFieldNumber("f8"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.ROOK, true));
+            givenSituation.put(Field.getFieldNumber("g8"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.KING, true));
+            givenSituation.put(Field.getFieldNumber("a7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("b7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.BISHOP, true));
+            givenSituation.put(Field.getFieldNumber("c7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("f7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("h7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("b6"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            givenSituation.put(Field.getFieldNumber("g6"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.PAWN, true));
+            
+            /*
+             * Spielsituation, bei der der Gegner nach einem Zug Schachmatt ist.
+             * 
             givenSituation.put(Field.getFieldNumber("b7"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.QUEEN, true));
             givenSituation.put(Field.getFieldNumber("g6"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.WHITE, ChessfigureConstants.KING, true));
             
             givenSituation.put(Field.getFieldNumber("h8"), ChessfigureConstants.makeFigureByte(ChessfigureConstants.BLACK, ChessfigureConstants.KING, true));
+            */
+            
             
             /*
             Field field = Field.getInstance();
